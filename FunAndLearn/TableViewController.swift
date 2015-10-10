d4ef10129a126660ff647e5af2c05f56f5e13751
Doug Harper <doug@zewkey.com>
2015-10-09 21:38:42 -0500

    Updated to Load JSON from URL

--- FunAndLearn/TableViewController.swift
+++ FunAndLearn/TableViewController.swift
@@ -8,56 +8,39 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class TableViewController: UITableViewController, NSURLConnectionDelegate {
     
     var dataTitle = [String]()
     var dataDescription = [String]()
     
     lazy var data = NSMutableData()
-    
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         startConnection()
         
-        
-       
-        
-        if let path = NSBundle.mainBundle().pathForResource("data", ofType: "plist") {
-            if let arrayOfDictionaries = NSArray(contentsOfFile: path) {
-                for dict in arrayOfDictionaries {
-                    dataTitle.append(dict.objectForKey("title") as! String)
-                    dataDescription.append(dict.objectForKey("description") as! String)
-                }
-            }
-        }
-        
-//        Check to see the data is available in console
-//        print(dataTitle.description)
-//        print(dataDescription.description)
-        
     }
-
-    // MARK: - Table view data source
-
+    
+    // MARK: - TableView
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return dataTitle.count
+        
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
+        
         cell.textLabel!.text = dataTitle[indexPath.row]
         cell.detailTextLabel!.text = dataDescription[indexPath.row]
-
+        
         return cell
     }
     
@@ -67,13 +50,12 @@ class TableViewController: UITableViewController {
     }
     
     //: MARK JSON from URL
-    
 
     func startConnection() {
         let urlPath: String = "http://www.mocky.io/v2/560920cc9665b96e1e69bb46"
-        var url: NSURL = NSURL(string: urlPath)!
-        var request: NSURLRequest = NSURLRequest(URL: url)
-        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
+        let url: NSURL = NSURL(string: urlPath)!
+        let request: NSURLRequest = NSURLRequest(URL: url)
+        let connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
         connection.start()
     }
     
@@ -82,23 +64,19 @@ class TableViewController: UITableViewController {
     }
     
     func connectionDidFinishLoading(connection: NSURLConnection!) {
-        let err: NSError
-        // throwing an error on the line below (can't figure out where the error message is)
         
         do {
         let jsonResult: NSArray = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as! NSArray
-        print(jsonResult)
-
+        
+            // print(jsonResult)
+            for dict in jsonResult {
+                dataTitle.append(dict.objectForKey("title") as! String)
+                dataDescription.append(dict.objectForKey("description") as! String)
+                self.tableView.reloadData()
+            }
             
         } catch{
             print("error")
         }
-        
     }
-   
-    
-    
-    
-    
-    
 }
